e6122828357e4de58d37a8932288cb08f12588db
carlosvalverderojas <desarrollowebpavas@gmail.com>
2025-12-30 19:53:03 -0600

    Disable ProductoIndexer on render profile

--- cliente-service/src/main/java/com/unir/cliente/cliente_service/service/ProductoIndexer.java
+++ cliente-service/src/main/java/com/unir/cliente/cliente_service/service/ProductoIndexer.java
@@ -5,10 +5,12 @@ import com.unir.cliente.cliente_service.elastic.ProductoElasticRepository;
 import com.unir.cliente.cliente_service.model.Producto;
 import com.unir.cliente.cliente_service.repository.ProductoRepository;
 import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Profile("!render") // En Render NO se crea este bean
 @Component
 public class ProductoIndexer {
 
@@ -16,7 +18,7 @@ public class ProductoIndexer {
     private final ProductoElasticRepository elasticRepository;
 
     public ProductoIndexer(ProductoRepository productoRepository,
-                            ProductoElasticRepository elasticRepository) {
+                           ProductoElasticRepository elasticRepository) {
         this.productoRepository = productoRepository;
         this.elasticRepository = elasticRepository;
     }
